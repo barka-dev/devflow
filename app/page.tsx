1054a9d902ce274f4b46824ac596f3742e01c4c4
Abdel <abdelbarka96@gmail.com>
2024-12-21 21:54:54 +0100

    configure Theming and Chadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,29 @@
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  // DropdownMenuLabel,
+  // DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
 const Home = () => {
-  return <h1 className="text-3xl font-black text-white">Home</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-black text-purple-400 dark:text-white">Home</h1>
+      <DropdownMenu>
+        <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+        <DropdownMenuContent>
+          {/* <DropdownMenuLabel>My Account</DropdownMenuLabel> */}
+          {/* <DropdownMenuSeparator /> */}
+          <DropdownMenuItem>Dark</DropdownMenuItem>
+          <DropdownMenuItem>Light</DropdownMenuItem>
+          <DropdownMenuItem>System</DropdownMenuItem>
+          {/* <DropdownMenuItem>Subscription</DropdownMenuItem> */}
+        </DropdownMenuContent>
+      </DropdownMenu>
+    </>
+  )
 };
 
 export default Home;
